f7bcfca4dfd56b7df5a1eb1b7a01a67907144f1f
Anya <81263768+anyabuu@users.noreply.github.com>
2025-03-16 17:59:42 +0200

    names of variables fixed to correct

--- src/index.js
+++ src/index.js
@@ -1,5 +1,54 @@
-let do = prompt('Enter your name');
-const if = prompt('Enter your last name');
-var while = prompt('Enter your age');
+function askInfo(message) {
+  let name;
 
-alert(`Hi ${do} ${if}, your age is ${while}`);
+  do {
+    name = prompt(message);
+
+    if (name === null) {
+      return null;
+    }
+  } while (name.trim() === '' || /\d/.test(name));
+
+  return name;
+}
+
+function askInfoNumber(message) {
+  let number;
+
+  do {
+    number = prompt(message);
+
+    if (number === null) {
+      return null;
+    }
+  } while (isNaN(number) || number.trim() === '');
+
+  return +number;
+}
+
+
+function questioner(){
+  const name = askInfo('Enter your name');
+  if(name === null){
+    alert('You cancelled')
+    return
+  }
+
+  const lastName = askInfo('Enter your last name');
+  if(lastName === null){
+    alert('You cancelled')
+    return
+  }
+
+  const age = askInfoNumber('Enter your age');
+  if(age === null){
+    alert('You cancelled')
+    return
+  }
+
+  alert(`Hi ${name} ${lastName}, your age is ${age}`);
+
+}
+
+questioner()
+
